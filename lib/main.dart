4ccb09c4303116ef759ed3146578a811e6e13df5
Doom_hopes <diadia1911@ukr.net>
2021-01-14 11:07:23 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DragTarget(
+                  builder: (BuildContext context, List<String> accepted,
+                      List<dynamic> rejected) {
+                    return new Container(
+                        width: 200, height: 200, color: Colors.lightBlue);
+                  },
+                  onAccept: (data) => print(data)),
+              Container(height: 50),
+              Draggable(
+                  data: "I was dragged",
+                  child: Container(width: 100, height: 100, color: Colors.red),
+                  feedback:
+                      Container(width: 100, height: 100, color: Colors.yellow))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
